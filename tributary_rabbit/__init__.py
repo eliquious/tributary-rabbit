bc436a62d8761b0a50f7acea40cccaefc7cb568b
Max Franks <max.franks@synapse-wireless.com>
2014-09-16 17:34:44 -0500

    version 1.0.0; working producer/consumer

--- tributary_rabbit/__init__.py
+++ tributary_rabbit/__init__.py
@@ -1,19 +1,143 @@
+# tributary dependencies
+from tributary.core import Message, Engine
+from tributary.streams import StreamElement, StreamProducer
+
+# amqp dependencies
+from haigha.connection import Connection
+from haigha.message import Message as AMQPMessage
+
+# other imports
+import json
+
+# exports
+__all__ = ['AMQPMessenger', 'AMQPListener', 'JSONListener', 'StringListener']
 
 
-if __name__ == '__main__':
-    from haigha.connection import Connection
-    from haigha.message import Message
+class AMQPMessenger(AMQPMessenger):
+    """docstring for AMQPMessenger"""
+    def __init__(self, name, user, password, vhost, host, exchange, queue,
+                 routing_key, exchange_type='topic', auto_delete=False,
+                 heartbeat=None, debug=False):
+        super(AMQPMessenger, self).__init__(name)
 
-    connection = Connection(
-      user='guest', password='guest',
-      vhost='/', host='localhost',
-      heartbeat=None, debug=True)
+        # amqp connection details
+        self.user = user
+        self.password = password
+        self.vhost = vhost
+        self.host = host
+        self.exchange = exchange
+        self.queue = queue
+        self.routing_key = routing_key
+        self.heartbeat = heartbeat
+        self.debug = debug
 
-    ch = connection.channel()
-    ch.exchange.declare('test_exchange', 'direct')
-    ch.queue.declare('test_queue', auto_delete=True)
-    ch.queue.bind('test_queue', 'test_exchange', 'test_key')
-    ch.basic.publish( Message('body', application_headers={'hello':'world'}),
-      'test_exchange', 'test_key' )
-    print ch.basic.get('test_queue')
-    connection.close()
+        # options
+        self.exchange_type = exchange_type
+        self.auto_delete = auto_delete
+
+    def preProcess(self, msg):
+        """Called when the actor starts"""
+
+        # open connection
+        self.conn = Connection(
+            user=self.user, password=self.password,
+            vhost=self.vhost, host=self.host,
+            heartbeat=self.heartbeat, debug=self.debug)
+
+        # create AMQP channel
+        self.channel = self.conn.channel()
+        self.channel.exchange.declare(self.exchange, self.exchange_type)
+        self.channel.queue.declare(self.queue, self.auto_delete)
+        self.channel.queue.bind(self.queue, self.exchange, self.routing_key)
+
+    def postProcess(self, message):
+        """Called when actor is stopped or killed"""
+        self.conn.close()
+
+    def process(self, message):
+        """Publish message to queue"""
+        if self.debug:
+            self.log("Publishing: " + str(message.data))
+        self.channel.basic.publish(
+            AMQPMessage(str(message.data)),
+            self.exchange, self.routing_key)
+
+
+class AMQPListener(StreamProducer):
+    """docstring for AMQPListener"""
+    def __init__(self, name, user, password, vhost, host, exchange, queue,
+                 routing_key, exchange_type='topic', auto_delete=False,
+                 heartbeat=None, debug=False):
+        super(AMQPListener, self).__init__(name)
+
+        # amqp connection details
+        self.user = user
+        self.password = password
+        self.vhost = vhost
+        self.host = host
+        self.exchange = exchange
+        self.queue = queue
+        self.routing_key = routing_key
+        self.heartbeat = heartbeat
+        self.debug = debug
+
+        # options
+        self.exchange_type = exchange_type
+        self.auto_delete = auto_delete
+
+    def preProcess(self, msg):
+        """Called when the actor starts"""
+
+        # open connection
+        self.conn = Connection(
+            user=self.user, password=self.password,
+            vhost=self.vhost, host=self.host,
+            heartbeat=self.heartbeat, debug=self.debug)
+
+        # create AMQP channel
+        self.channel = self.conn.channel()
+        self.channel.exchange.declare(self.exchange, self.exchange_type)
+        self.channel.queue.declare(self.queue, self.auto_delete)
+        self.channel.queue.bind(self.queue, self.exchange, self.routing_key)
+
+    def postProcess(self, message):
+        """Called when actor is stopped or killed"""
+        self.conn.close()
+
+    def parse(self, data):
+        """Parses data received"""
+        raise NotImplementedError("parse")
+
+    def process(self, message=None):
+        """Recieves message from queue"""
+
+        while self.running:
+            message = self.channel.basic.get(self.queue)
+            if message:
+                content = message.body
+
+                # log message
+                if self.debug:
+                    self.log("Recieved: " + str(content))
+
+                # send to child nodes
+                self.scatter(Message(**self.parse(content)))
+            else:
+                # yield to other greenlet
+                self.tick()
+
+
+class JSONListener(AMQPListener):
+    """docstring for JSONListener"""
+    def parse(self, data):
+        msg = json.loads(str(data))
+        if type(msg) == dict:
+            return msg
+        else:
+            return {'body': msg}
+
+
+class StringListener(AMQPListener):
+    """docstring for StringListener"""
+    def parse(self, data):
+        return {'body': data}
